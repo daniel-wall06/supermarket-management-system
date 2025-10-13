33147a1ddfb8cb372928b755d5d0cfbbd41d7696
Daniel Wall <20112377@mail.wit.ie>
2025-10-13 21:45:10 +0100

    Changed parameter to generic, added node links, and added isEmpty and getHead methods

--- src/main/java/LinkedList/DoublyLinkedList.java
+++ src/main/java/LinkedList/DoublyLinkedList.java
@@ -1,36 +1,43 @@
 package LinkedList;
 
-public class DoublyLinkedList{
-    private Node head;
-    private Node tail;
+public class DoublyLinkedList<T>{
+    private Node<T> head;
+    private Node<T> tail;
 
     public boolean isEmpty(){
         return head == null; //checks if the list is empty, if the head is null list is empty
     }
 
-    public void insertAtHead(Node data){
-        Node newNode = new Node(data);
+    public void insertAtHead(T data){
+        Node<T> newNode = new Node<T>(data);
         if(isEmpty()){
             head = tail = newNode;
         }
         else{
             newNode.setNext(head);
+            head.setPrevious(newNode);
             head = newNode;
         }
 
 
     }
-
-    public void insertAtTail(Node data){
-        Node newNode = new Node(data);
+    public void insertAtTail(T data){
+        Node<T> newNode = new Node<T>(data);
         if(isEmpty()){
             head = tail = newNode;
         }
         else{
             tail.setNext(newNode);
+            newNode.setPrevious(tail);
             tail = newNode;
         }
     }
+
+    public Node<T> getHead() {
+        return head;
+    }
+
+
 
 
 }

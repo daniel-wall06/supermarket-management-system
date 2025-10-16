ef485c975960bfff1cfe6af24093763f58c910b0
Daniel Wall <20112377@mail.wit.ie>
2025-10-16 22:49:39 +0100

    Added getters and setters and JavaDocs to Goods class.

--- src/main/java/models/Good.java
+++ src/main/java/models/Good.java
@@ -1,6 +1,9 @@
 package models;
 import LinkedList.DoublyLinkedList;
 
+/**
+ *
+ */
 public class Good {
     private String description;
     private int weight;
@@ -9,6 +12,13 @@ public class Good {
     private String Temperature;
     private String photoURL;
 
-    public Good(String description, int weight, int quantityToAdd, String temperature, String photoURL) {}
+    public Good(String description, int weight, int quantityToAdd, String temperature, String photoURL) {
+        this.description = description;
+        this.weight = weight;
+        this.quantityToAdd = quantityToAdd;
+        this.Temperature = temperature;
+        this.photoURL = photoURL;
+    }
+
 
 }

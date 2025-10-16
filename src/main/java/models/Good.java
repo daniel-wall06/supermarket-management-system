9eed796c54d21790a6459a594c0f857894c1c3ae
Daniel Wall <20112377@mail.wit.ie>
2025-10-16 18:59:29 +0100

    Added Good class with fields and constructor defined

---
+++ src/main/java/models/Good.java
@@ -0,0 +1,14 @@
+package models;
+import LinkedList.DoublyLinkedList;
+
+public class Good {
+    private String description;
+    private int weight;
+    private int unitPrice;
+    private int quantityToAdd;
+    private String Temperature;
+    private String photoURL;
+
+    public Good(String description, int weight, int quantityToAdd, String temperature, String photoURL) {}
+
+}

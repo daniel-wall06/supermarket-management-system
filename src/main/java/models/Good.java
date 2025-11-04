b5721a723b621638c3cb24d3069f6927f548a018
Daniel Wall <20112377@mail.wit.ie>
2025-11-04 15:38:49 +0000

    Added getters and setters and toString method to good

--- src/main/java/models/Good.java
+++ src/main/java/models/Good.java
@@ -20,5 +20,56 @@ public class Good {
         this.photoURL = photoURL;
     }
 
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public int getWeight() {
+        return weight;
+    }
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
 
+    public int getQuantityToAdd() {
+        return quantityToAdd;
+    }
+    public void setQuantityToAdd(int quantityToAdd) {
+        this.quantityToAdd = quantityToAdd;
+    }
+    public String getTemperature() {
+        return Temperature;
+    }
+    public void setTemperature(String temperature) {
+        Temperature = temperature;
+    }
+    public String getPhotoURL() {
+        return photoURL;
+    }
+    public void setPhotoURL(String photoURL) {
+        this.photoURL = photoURL;
+    }
+    public int getUnitPrice() {
+        return unitPrice;
+    }
+    public void setUnitPrice(int unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+    public double getTotalPrice() {
+        return unitPrice * quantityToAdd;
+    }
+
+    @Override
+    public String toString() {
+        return "Good{" +
+                "description='" + description + '\'' +
+                ", weight=" + weight +
+                ", unitPrice=" + unitPrice +
+                ", quantityToAdd=" + quantityToAdd +
+                ", Temperature='" + Temperature + '\'' +
+                ", photoURL='" + photoURL + '\'' +
+                '}';
+    }
 }

bbd7791c9554c313c4dfbaf3715639d03198925f
Daniel Wall <20112377@mail.wit.ie>
2025-11-04 15:39:52 +0000

    Added javaDoc comments and list method to shelf

--- src/main/java/models/Shelf.java
+++ src/main/java/models/Shelf.java
@@ -1,30 +1,79 @@
 package models;
 import LinkedList.DoublyLinkedList;
+import LinkedList.Node;
 
-
+/**
+ *
+ */
 public class Shelf {
     private int shelfNumber;
     private DoublyLinkedList<Good> goodsList;
 
-    public Shelf(int shelfNumber, DoublyLinkedList<Good> goodsList) {
+    /**
+     *
+     * @param shelfNumber
+     */
+    public Shelf(int shelfNumber) {
         this.shelfNumber = shelfNumber;
-        this.goodsList = goodsList;
+        this.goodsList = new DoublyLinkedList<>();
+
     }
+
+    /**
+     *
+     * @return
+     */
     public int getShelfNumber() {
         return shelfNumber;
     }
+
+    /**
+     *
+     * @return
+     */
     public DoublyLinkedList<Good> getGoodsList() {
         return goodsList;
     }
+
+    /**
+     *
+     * @param goodsList
+     */
     public void setGoodsList(DoublyLinkedList<Good> goodsList) {
         this.goodsList = goodsList;
     }
+
+    /**
+     *
+     * @param shelfNumber
+     */
     public void setShelfNumber(int shelfNumber) {
         this.shelfNumber = shelfNumber;
     }
+
     // TODO: Update method to check if the goods details are already stored etc
     public void addGood(Good good) {
         goodsList.insertAtTail(good);
+    }
+    //FIXME: FIX THIS
+    public String allShelfItems() {
+        String shelfString = "";
+        if(goodsList != null && !goodsList.isEmpty()) {
+            Node<Good> current = goodsList.getHead();
+            while(current != null) {
+                Good good = current.getValue();
+                shelfString += good.toString() + "\n";
+                current = current.getNext();
+            }
+        }
+        else{
+            shelfString += "No goods on the shelf \n";
+        }
+
+        shelfString += "Shelf Number: " + shelfNumber
+                + " | Total Goods: " + (goodsList != null ? goodsList.getSize() : 0)
+                + "\n";
+        return shelfString;
     }
 
 
